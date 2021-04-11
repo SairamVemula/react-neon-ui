8184b823f04113b2e4758e01e78fc194f4647b0a
svemula <svemula@inspeero.com>
2021-04-11 20:29:30 +0530

    select empty issues

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -66,14 +66,13 @@ class Select extends React.Component<SelectProps, any> {
         target.style.boxShadow = 'none';
     };
     onClick = (): void => {
-        console.log(this.state.open)
-        if (this.state.open) {
+        if (this.props.list && this.props.list?.length > 0 && this.state.open) {
             this.setState({
                 open: !this.state.open,
                 listStyle: { ...this.state.listStyle, transition: 'height 2s', height: 0, display: 'none' }
             })
         }
-        else {
+        else if (this.props.list && this.props.list?.length > 0 && !this.state.open) {
             this.setState({
                 open: !this.state.open,
                 listStyle: { ...this.state.listStyle, transition: 'height 2s', height: 320, display: 'block' }
@@ -91,23 +90,25 @@ class Select extends React.Component<SelectProps, any> {
         return (
             <div className={styles.main} style={{ color: this.sltColor.primary, border: `solid 2px ${this.sltColor.primary} `, background: background || '#031321' }} onClick={this.onClick}>
                 <p className={styles.p} >{this.state.placeholder || 'Select from List'}</p>
-                <svg className={styles.svg} version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" width={"20"} height={"20"} viewBox="0 0 292.362 292.362" style={{ fill: this.sltColor.primary }}>
+                <svg className={`${styles.svg} ${this.state.open ? styles.svg1 : styles.svg2}`} version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" width={"20"} height={"20"} viewBox="0 0 292.362 292.362" style={{ fill: this.sltColor.primary }}>
                     <g>
                         <path className={styles.svg} d="M286.935,69.377c-3.614-3.617-7.898-5.424-12.848-5.424H18.274c-4.952,0-9.233,1.807-12.85,5.424
 		C1.807,72.998,0,77.279,0,82.228c0,4.948,1.807,9.229,5.424,12.847l127.907,127.907c3.621,3.617,7.902,5.428,12.85,5.428
 		s9.233-1.811,12.847-5.428L286.935,95.074c3.613-3.617,5.427-7.898,5.427-12.847C292.362,77.279,290.548,72.998,286.935,69.377z"/>
                     </g>
                 </svg>
-                <div className={styles.list} style={this.state.listStyle}>
-                    {
-                        this.props.list?.map(a => <a className={styles.listItem} style={{ color: this.sltColor.primary, borderColor: this.sltColor.primary }}
-                            key={a}
-                            onMouseEnter={this.onMouseEnter}
-                            onMouseLeave={this.onMouseLeave}
-                            onClick={() => this.onSelect(a)}
-                        >{a}</a>)
-                    }
-                </div>
+                {this.props.list && this.props.list?.length > 0 ?
+                    <div className={styles.list} style={this.state.listStyle}>
+                        {
+                            this.props.list?.map(a => <a className={styles.listItem} style={{ color: this.sltColor.primary, borderColor: this.sltColor.primary }}
+                                // arr.map(a => <a className={styles.listItem} style={{ color: this.sltColor.primary, borderColor: this.sltColor.primary }}
+                                key={a}
+                                onMouseEnter={this.onMouseEnter}
+                                onMouseLeave={this.onMouseLeave}
+                                onClick={() => this.onSelect(a)}
+                            >{a}</a>)
+                        }
+                    </div> : ''}
             </div >
         )
     }
